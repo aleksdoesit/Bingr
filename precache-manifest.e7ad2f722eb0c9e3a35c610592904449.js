0ab5953bffaff13d4421cefb9b7b1e3f91ce668b
Aleks <nikolic.aleks90@gmail.com>
2020-05-25 15:30:24 -0400

    Updates

---
+++ precache-manifest.e7ad2f722eb0c9e3a35c610592904449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03e20a0fb92fd9636d331a44b476f48",
+    "url": "/Bingr/index.html"
+  },
+  {
+    "revision": "9352b54ae91153759b40",
+    "url": "/Bingr/static/css/main.3bcc6986.chunk.css"
+  },
+  {
+    "revision": "2c94f0417e2e23c75eb0",
+    "url": "/Bingr/static/js/2.81f9a8a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bingr/static/js/2.81f9a8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9352b54ae91153759b40",
+    "url": "/Bingr/static/js/main.0f66e8ca.chunk.js"
+  },
+  {
+    "revision": "0fd69c5da170a8f46422",
+    "url": "/Bingr/static/js/runtime-main.1b78dc4b.js"
+  }
+]);
